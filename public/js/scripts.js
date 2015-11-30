2d966f8ee7cfd320168d917354ce5caa41ef221e
Taylor Growden <taylor.growden@gmail.com>
2015-11-29 20:29:33 -0500

    update shit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,86 +1,71 @@
-var index = 2;
-function insertIngredient(){
-            var table=document.getElementById("ingredients");
-            var row=table.insertRow(table.rows.length);
-            var cell1=row.insertCell(0);
-            var t1=document.createElement("span");
-                t1.id = "txtIngredient"+index;
-                t1.innerHTML = index+".";
-                cell1.appendChild(t1);
-  
-            var cell2=row.insertCell(1);
-            var t2=document.createElement("input");
-                t2.id = "txtIngredient"+index;
-                cell2.appendChild(t2);
-  
-            var cell3=row.insertCell(2);
-            var div3 = document.createElement("div");
-                div3.className = "input-group";
-            var span3 = document.createElement("span");
-                span3.className="input-group-addon";
-                span3.innerHTML = '%';
-            var t3=document.createElement("input");
-                t3.id = "txtPercent"+index;
-                t3.type = "number";
-            cell3.appendChild(div3);
-            div3.appendChild(t3);
-            div3.appendChild(span3);
+// IIFE
+(function() {
+  // shorthand for $(document).ready
+  $(function() {
+    var numIngredient = 2;
+    $("#addRow").click(function() {
+      $("#ingredients tr:last").clone().find('input').val('').end().insertAfter("#ingredients tr:last");
+      $("#ingredients tr:last").find('.numIngredient').html(numIngredient + ".");
+      numIngredient++;
+    });
+    var deleteIngredient = function deleteIngredient() {
+      var table = document.getElementById("ingredients");
+      var count = $('#ingredients tr').length;
+      if (count > 2) {
+        var row = table.deleteRow(-1);
+        numIngredient--;
+      }
+    };
+    $("#deleteRow").click(deleteIngredient);
+    var massCheck = function massCheck() {
+      var massTd = document.getElementsByClassName('mass');
+      var massHeader = document.getElementsByClassName('massHeader');
+      var flavorMass = document.getElementsByClassName('flavorMass');
+      if ($('#massCheck').is(':checked')) {
+        $(massTd).removeClass('hide');
+        $(massHeader).removeClass('hide');
+        var massInput = document.createElement("input");
+        massInput.className = "massInput";
+        massInput.type = "number";
+        var massSpan = document.createElement("span");
+        massSpan.className = "input-group-addon";
+        massSpan.innerHTML = "g/mL";
+        $(flavorMass).append(massInput);
+        $(flavorMass).append(massSpan);
+      } else if (!$('#massCheck').is(':checked')) {
+        $(massTd).addClass('hide');
+        $(massHeader).addClass('hide');
+        $(flavorMass).empty();
+      }
+    };
+    $('#massCheck').change(massCheck);
 
-            var cell4=row.insertCell(3);
-            var div=document.createElement("div");
-              div.className = "input-group flavorMass";
-            $(cell4).append(div);
+    var calculate = function calculate(){
+      var targetNic = $("#targetNic").val(),
+          targetVG = $("#targetVG").val(),
+          targetPG = $("#targetPG").val(),
+          total = $("#total").val();
+      $('#output').removeClass("hide");
+      $('#output > tbody > tr').each(function(){
+        var targets = new Object();
+        targets.identity = $("td", this).first().text();
+        targets.percent = $(this).find("input").valueOf();
+        console.log("output " + targets.identity + " " + targets.percent);
 
-            if ( $('#massCheck').is(':checked') ) {
-              $('.flavorMass').each(function(){
-                if ($.trim($(this).text()).length === 0){
-                  var input = document.createElement("input");
-                    input.className = "massInput";
-                    input.type = "number";
-                  var span = document.createElement("span");
-                    span.className = "input-group-addon";
-                    span.innerHTML ="g/mL";
-                  $(this).append(input, span);
-                }
-              });
-
-            }
-
-  
-      index++;
-
-}
-
-function deleteIngredient() {
-var table = document.getElementById("ingredients");
-
-var count = $('#ingredients tr').length;
-if ( count > 2){
-  var row = table.deleteRow(-1);
-  index--;
-}
-}
+      });
+      
 
 
-function massCheck(){
-  var massTd = document.getElementsByClassName('mass');
-  var massHeader = document.getElementsByClassName('massHeader');
-  var flavorMass = document.getElementsByClassName('flavorMass');
-  if ( $('#massCheck').is(':checked') ) {
-      $(massTd).removeClass('hide');
-      $(massHeader).removeClass('hide');
-      var massInput=document.createElement("input");
-      massInput.className = "massInput";
-      massInput.type = "number";
-      var massSpan = document.createElement("span");
-      massSpan.className = "input-group-addon";
-      massSpan.innerHTML = "g/mL";
-      $(flavorMass).append(massInput);
-      $(flavorMass).append(massSpan);
 
-  } else if ( ! $('#massCheck').is(':checked') ){
-      $(massTd).addClass('hide');
-      $(massHeader).addClass('hide');
-      $(flavorMass).empty();
-  }
-}
+      if ($('#massCheck').is(':checked')) {
+        var massNic = $("#massNic").val(),
+            massVG = $("#massVG").val(),
+            massPG = $("#massPG").val();
+      }
+
+    };
+    $('#calculate').click(calculate);
+
+    
+  });
+}());
